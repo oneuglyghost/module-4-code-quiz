444caffd24e3dd7043b8ca1fa55d5ef2feff5381
Bernardo Alonso <bernardo4430@gmail.com>
2023-12-06 11:06:27 -0600

    added first question finished simple javascript to switch to next question when picking an answer

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
-const pages =["question1.html"]
+//array of question URLs
+const pages =["question1.html","question2.html","question3.html","question4.html","question5.html","question6.html","question7.html","question8.html","question9.html","question10.html",]
+
+// counter to keep track of the current question
+let currentQuestionIndex = 0;
+
+// update question number element
+function updateQuestionNumber() {
+    document.getElementById("questionNumber").textContent
+}
+
+//check if there are more questions
+function nextQuestion() {
+    if (currentQuestionIndex < pages.lenth -1){
+        currentQuestionIndex++;
+        updateQuestionNumber();
+        // go to next question
+        window.location.href = pages[currentQuestionIndex];
+    } else {
+        // change to score screen
+        alert('last question');
+    }   
+}
+updateQuestionNumber();
